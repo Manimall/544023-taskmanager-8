2d30ca17d2a040abd8cd92b823781b3850034b5e
Manimall <gorpunov94@mail.ru>
2019-02-23 02:50:57 +0300

    my mistake - i"ve forgot to add to git new created file

---
+++ src/filters-data.js
@@ -0,0 +1,44 @@
+// Импорт относительно текущего модуля
+import {getRandomNumber} from './helpers.js';
+
+// Массив с данными фильтров, содержащий следующие данные, для каждого фильтра:
+// id фильтра (строка), amount - кол-во элементов в фильтре, атрибут checked (по умолчанию false), аттрибут disabled (по умолчанию false)
+const filters = [
+  {
+    id: `All`,
+    amount: getRandomNumber(),
+    isChecked: false,
+    isDisabled: false
+  },
+  {
+    id: `Overdue`,
+    amount: getRandomNumber(),
+    isChecked: false,
+    isDisabled: true
+  },
+  {
+    id: `Today`,
+    amount: getRandomNumber(),
+    isChecked: true
+  },
+  {
+    id: `Favorites`,
+    amount: getRandomNumber()
+  },
+  {
+    id: `Repeating`,
+    amount: getRandomNumber(),
+    isDisabled: true
+  },
+  {
+    id: `Tags`,
+    amount: getRandomNumber(),
+    isChecked: true
+  },
+  {
+    id: `Archive`,
+    amount: getRandomNumber()
+  }
+];
+
+export {filters};
