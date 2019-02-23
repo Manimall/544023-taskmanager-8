43346c9432331e499852274d772355d758db8be4
Manimall <gorpunov94@mail.ru>
2019-02-23 16:56:54 +0300

    changes to tutor"s remark

--- src/helpers.js
+++ src/helpers.js
@@ -4,8 +4,6 @@
  * @param {number} [max=10] - максимальное кол-во задач
  * @return {number} - случайное число - (кол-во тасков у любого фультра)
  */
-const getRandomNumber = (min = 0, max = 10) => {
+export const getRandomNumber = (min = 0, max = 10) => {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
-
-export {getRandomNumber};
